33247f52132820ca622ddafbe705196cb2052b74
Paul Sikorski <roflpauli@hotmail.de>
2021-11-05 10:57:54 +0100

    Renamed FilenameFilter

--- src/main/java/de/paulsikorski/calcitetest/CalciteTestFilenameFilter.java
+++ src/main/java/de/paulsikorski/calcitetest/CalciteTestFilenameFilter.java
@@ -3,7 +3,7 @@ package de.paulsikorski.calcitetest;
 import java.io.File;
 import java.io.FilenameFilter;
 
-public class SqlOnFileFilenameFilter implements FilenameFilter {
+public class CalciteTestFilenameFilter implements FilenameFilter {
 
 	public boolean accept(File dir, String name) {
 		// TODO Auto-generated method stub
