ff5898692ae4c9396357c214aab1fc3d6af8ac2e
Paul Sikorski <roflpauli@hotmail.de>
2021-11-07 14:48:02 +0100

    Connection now  managed by try-with-resource block, also some formatting

--- src/main/java/de/paulsikorski/calcitetest/TestLauncher.java
+++ src/main/java/de/paulsikorski/calcitetest/TestLauncher.java
@@ -26,69 +26,69 @@ public class TestLauncher {
 		
 		//Create a connection to calcite
 		LOG.info("Creating a connection to calcite");
-		Connection connection =
-		    DriverManager.getConnection("jdbc:calcite:", info);
-		CalciteConnection calciteConnection =
-		    connection.unwrap(CalciteConnection.class);
 		
-		//SELECT on both tables
-		final String sql1 = new StringBuilder().append("select * from ")
-				.append("\"STUDENTS.json\"")
-				.toString();
-		try(Statement statement = calciteConnection.createStatement();
-			ResultSet selectStudentsResult = calciteConnection.createStatement().executeQuery(sql1)) {
+		try(Connection connection = DriverManager.getConnection("jdbc:calcite:", info);
+				CalciteConnection calciteConnection = connection.unwrap(CalciteConnection.class);){
 			
-			ResultSetMetaData rSMD = selectStudentsResult.getMetaData();
-			int columnsNumber2 = rSMD.getColumnCount();
-			   while (selectStudentsResult.next()) {
-			       for (int i = 1; i <= columnsNumber2; i++) {
-			           if (i > 1) System.out.print(",  ");
-			           String columnValue = selectStudentsResult.getString(i);
-			           System.out.print(columnValue + " " + rSMD.getColumnName(i));
-			       }
-			   System.out.println("");
-			}
-		} 
-		
-		final String sql2 = new StringBuilder().append("select * from ")
-				.append("\"TOPICS.json\"")
-				.toString();
-		try(Statement statement = calciteConnection.createStatement();
-				ResultSet selectStudentsResult = statement.executeQuery(sql2)) {
-			
-			ResultSetMetaData rSMD = selectStudentsResult.getMetaData();
-			int columnsNumber2 = rSMD.getColumnCount();
-			   while (selectStudentsResult.next()) {
-			       for (int i = 1; i <= columnsNumber2; i++) {
-			           if (i > 1) System.out.print(",  ");
-			           String columnValue = selectStudentsResult.getString(i);
-			           System.out.print(columnValue + " " + rSMD.getColumnName(i));
-			       }
-			   System.out.println("");
-			}
-		}
-		
-		//Create and execute statement
-		LOG.info("Creating and executing join statement");
-		final String sql3 = new StringBuilder().append("select ")
-				.append("\"STUDENTS.json\".matNr, \"STUDENTS.json\".name, \"TOPICS.json\".topic from ")
-				.append("\"STUDENTS.json\" inner join \"TOPICS.json\" on ")
-				.append("\"STUDENTS.json\".matNr=\"TOPICS.json\".matNr")
-				.toString();
-		try(Statement statement = calciteConnection.createStatement();
-			ResultSet queryJoinResult = statement.executeQuery(sql3)) {
-			
-			ResultSetMetaData rSMD = queryJoinResult.getMetaData();
-			while (queryJoinResult.next()) {
-				for (int i = 1; i <= rSMD.getColumnCount(); i++) {
-		           if (i > 1) System.out.print(",  ");
-		           String columnValue = queryJoinResult.getString(i);
-		           System.out.print(columnValue + " " + rSMD.getColumnName(i));
+			//SELECT on both tables
+			final String sql1 = new StringBuilder().append("select * from ")
+					.append("\"STUDENTS.json\"")
+					.toString();
+			try(Statement statement = calciteConnection.createStatement();
+				ResultSet selectStudentsResult = calciteConnection.createStatement().executeQuery(sql1)) {
+				
+				final ResultSetMetaData rSMD = selectStudentsResult.getMetaData();
+				int columnsNumber2 = rSMD.getColumnCount();
+				   while (selectStudentsResult.next()) {
+				       for (int i = 1; i <= columnsNumber2; i++) {
+				           if (i > 1) System.out.print(",  ");
+				           String columnValue = selectStudentsResult.getString(i);
+				           System.out.print(columnValue + " " + rSMD.getColumnName(i));
+				       }
+				   System.out.println("");
 				}
-		    System.out.println("");
-	   }
+			} 
+			
+			final String sql2 = new StringBuilder().append("select * from ")
+					.append("\"TOPICS.json\"")
+					.toString();
+			try(Statement statement = calciteConnection.createStatement();
+					ResultSet selectStudentsResult = statement.executeQuery(sql2)) {
+				
+				final ResultSetMetaData rSMD = selectStudentsResult.getMetaData();
+				int columnsNumber2 = rSMD.getColumnCount();
+				   while (selectStudentsResult.next()) {
+				       for (int i = 1; i <= columnsNumber2; i++) {
+				           if (i > 1) System.out.print(",  ");
+				           String columnValue = selectStudentsResult.getString(i);
+				           System.out.print(columnValue + " " + rSMD.getColumnName(i));
+				       }
+				   System.out.println("");
+				}
+			}
+			
+			//Create and execute statement
+			LOG.info("Creating and executing join statement");
+			final String sql3 = new StringBuilder().append("select ")
+					.append("\"STUDENTS.json\".matNr, \"STUDENTS.json\".name, \"TOPICS.json\".topic from ")
+					.append("\"STUDENTS.json\" inner join \"TOPICS.json\" on ")
+					.append("\"STUDENTS.json\".matNr=\"TOPICS.json\".matNr")
+					.toString();
+			try(Statement statement = calciteConnection.createStatement();
+				ResultSet queryJoinResult = statement.executeQuery(sql3)) {
+				
+				final ResultSetMetaData rSMD = queryJoinResult.getMetaData();
+				while (queryJoinResult.next()) {
+					for (int i = 1; i <= rSMD.getColumnCount(); i++) {
+			           if (i > 1) System.out.print(",  ");
+			           String columnValue = queryJoinResult.getString(i);
+			           System.out.print(columnValue + " " + rSMD.getColumnName(i));
+					}
+			    System.out.println("");
+				}
+			}
 		}
-		connection.close();
+		
 	}
 	
 
